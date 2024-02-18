e5543a1d64c5d189d59a5f01bb10d247fc3e65ff
Shamsul Arifin <arifinbd6@gmail.com>
2024-02-18 16:22:12 +0600

    Added Delegates and callback functions

--- Plugins/QuadsSessions/Source/QuadsSessions/Private/MultipalyerSessionsSubsystem.cpp
+++ Plugins/QuadsSessions/Source/QuadsSessions/Private/MultipalyerSessionsSubsystem.cpp
@@ -4,12 +4,69 @@
 #include "MultipalyerSessionsSubsystem.h"
 
 #include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Online/OnlineSessionNames.h"
 
-UMultipalyerSessionsSubsystem::UMultipalyerSessionsSubsystem()
+UMultipalyerSessionsSubsystem::UMultipalyerSessionsSubsystem():
+	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete)),
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionCompelte)),
+	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
 	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
 	if (Subsystem)
 	{
 		SessionInterface = Subsystem->GetSessionInterface();
 	}
+}
+
+void UMultipalyerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::FindSessions(int32 MaxSearchResults)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SearchResult)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::DestroySession()
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::StartSession()
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::OnDestroySessionCompelte(FName SessionName, bool bWasSuccessful)
+{
+	
+}
+
+void UMultipalyerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	
 }
--- Plugins/QuadsSessions/Source/QuadsSessions/Public/MultipalyerSessionsSubsystem.h
+++ Plugins/QuadsSessions/Source/QuadsSessions/Public/MultipalyerSessionsSubsystem.h
@@ -18,8 +18,47 @@ class QUADSSESSIONS_API UMultipalyerSessionsSubsystem : public UGameInstanceSubs
 
 public:
 	UMultipalyerSessionsSubsystem();
+
+	/*
+	 * To handle session functionality. The menu class will call these
+	 */
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResults);
+	void JoinSession(const FOnlineSessionSearchResult& SearchResult);
+	void DestroySession();
+	void StartSession();
+	
 protected:
+	
+	/*
+	 * Internal callbacks for the delegates we'll add to the Online Session Interface delegate list.
+	 * This don't need to be called outside this class.
+	 */
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionCompelte(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	/*
+	 * To add to the Online Session Interface delegate list
+	 * We'll bind our MultiplayerSubsystem internal callbacks to these
+	 */
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FDelegateHandle FindSessionCompleteDelegateHandle;
+	
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
